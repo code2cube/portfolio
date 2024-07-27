77c81627994ff4465b51e068c60e8dfca2403275
Samuel Sharivker <sam@ShmulPro.local>
2024-07-26 23:55:53 -0400

    updated project cards

--- app/api/projects/route.tsx
+++ app/api/projects/route.tsx
@@ -6,33 +6,39 @@ export async function GET(req: Request) {
         description:
           "Grading App developed in React with Zi Xuan Yu and Benjamin Chau.",
         redirectPath: "/",
+        cardID: "rgCard",
       },
       {
         title: "PenguinImageAPI",
         description: "Rest API to get random images of Penguins.",
         redirectPath: "/",
+        cardID: "piaCard",
       },
       {
         title: "SEP10 Freedom Project",
         description:
           "Dental Website which was one of the top 3 projects in the SEP Expo 2023 for Grade 10.",
         redirectPath: "/",
+        cardID: "sep10Card",
       },
       {
         title: "LBH Blackhole Game",
         description:
           "Explore different parts of a blackhole in a 2D Unity Game.",
         redirectPath: "/",
+        cardID: "lbhbhgCard",
       },
       {
         title: "PigLatin Translator",
-        description: "Translate English into Piglatin and viceversa.",
+        description: "Translate English into Piglatin and vice versa.",
         redirectPath: "/",
+        cardID: "pltCard",
       },
       {
         title: "Ice Cube Bot",
         description: "Winter themed PVP Bot made for Snow Codes Bot Jam.",
         redirectPath: "/",
+        cardID: "placeholder",
       },
     ];
 
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -8,6 +8,7 @@ interface Project {
   title: string;
   description: string;
   redirectPath: string;
+  cardID: string;
 }
 
 export default function Projects() {
@@ -27,12 +28,26 @@ export default function Projects() {
     <div>
       <div className="container">
         <p className="text-center m-5 text-2xl sm:text-7xl">Projects</p>
-        <div className="justify-items-center m-auto gap-3 grid grid-cols-3">
+        <div className="justify-items-center m-auto gap-5 grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1">
           {data.map((item) => (
-            <div className="min-w-60 min-h-60 bg-slate-500 hover:bg-red-500 hover:scale-90 transition duration-300 ease-in hover:ease-in p-5 text-center flex flex-col items-center gap-3 justify-center rounded-md text-white">
-              <p className="text-4xl">{item.title}</p>
-              <p className="leading-4">{item.description}</p>
-              <Button asChild className="max-w-20 text-black" variant="outline">
+            <div
+              className="relative xl:w-96 md:w-56 sm:w-72 lg:w-72 w-80 my-5 hover:scale-90 transition-transform duration-300 ease-in p-5 text-center flex flex-col items-center gap-3 justify-center rounded-lg"
+              style={{
+                backgroundImage: `url('/img/projectCards/${item.cardID}.jpg')`,
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+              }}
+            >
+              <div className="absolute inset-0 bg-white opacity-80 text-black"></div>
+              <p className="relative z-10 xl:text-4xl lg:text-3xl text-2xl">
+                {item.title}
+              </p>
+              <p className="relative z-10 xl:leading-4">{item.description}</p>
+              <Button
+                asChild
+                className="relative z-10 max-w-20 text-black"
+                variant="outline"
+              >
                 <Link href={item.redirectPath}>View</Link>
               </Button>
             </div>
