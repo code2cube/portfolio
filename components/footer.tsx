13b17dbca5661701f6900b2a82bde2858a382932
Samuel Sharivker <sam@ShmulPro.local>
2024-07-27 20:43:46 -0400

    edited footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,7 +1,22 @@
+"use client";
+
+import Link from "next/link";
+
 export default function Footer() {
   return (
-    <div className="bg-black text-white p-5 text-center">
+    <footer className="bg-black text-white p-5 flex flex-col h-20 text-center justify-center items-center gap-1">
+      <div className="flex gap-3">
+        <Link href="https://github.com/code2cube/">
+          <i className="bi bi-github"></i>
+        </Link>
+        <Link href="https://www.instagram.com/live2cube">
+          <i className="bi bi-instagram"></i>
+        </Link>
+        <Link href="https://www.linkedin.com/in/samuel-sharivker/">
+          <i className="bi bi-linkedin"></i>
+        </Link>
+      </div>
       <p className="text-xs">Copyright © Samuel Sharivker 2024</p>
-    </div>
+    </footer>
   );
 }
