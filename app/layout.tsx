37fe62b2a1fdbfc51b6e730eea958db96d6edc10
Samuel Sharivker <sam@ShmulPro.local>
2024-07-28 00:56:16 -0400

    fixed favicon not working

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+      </head>
       <body className={inter.className}>
         <div className="flex flex-col min-h-screen">
           <Navbar />
