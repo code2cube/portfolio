607d9562485d2a7b5be8d28605658db23b42ea66
Samuel Sharivker <sjsharivker@gmail.com>
2024-07-30 14:43:24 -0400

    started using github api for project descriptions

--- app/api/projects/route.tsx
+++ app/api/projects/route.tsx
@@ -1,61 +1,94 @@
+import { Octokit } from "@octokit/rest";
+
+interface Project {
+  title: string;
+  cardID: string;
+  repo: string;
+  view?: string;
+  description?: string;
+}
+
 export async function GET(req: Request) {
   try {
-    const githubUsername = "code2cube";
+    const octokit = new Octokit({
+      auth: process.env.GITHUB_API_KEY,
+    });
 
-    const projects = [
+    const getDescription = async (
+      owner: string,
+      repo: string
+    ): Promise<string> => {
+      try {
+        const { data } = await octokit.repos.get({
+          owner,
+          repo,
+        });
+        return data.description ?? "No Description Found";
+      } catch (error) {
+        console.error(error);
+        return "No Description Found";
+      }
+    };
+
+    const projects: Project[] = [
       {
         title: "RiceGrades",
-        description:
-          "Grading App developed in React with Zi Xuan Yu and Benjamin Chau.",
         cardID: "rgCard",
-        repo: `https://github.com/${githubUsername}/RiceGrades`,
+        repo: "https://github.com/code2cube/RiceGrades",
         view: "ricegrades.pages.dev/",
       },
       {
         title: "PenguinImageAPI",
-        description: "Rest API to get random images of Penguins.",
         cardID: "piaCard",
-        repo: `https://github.com/${githubUsername}/PenguinImageAPI`,
+        repo: "https://github.com/code2cube/PenguinImageAPI",
         view: "https://penguin.sjsharivker.workers.dev/api",
       },
       {
         title: "SEP10 Freedom Project",
-        description:
-          "Dental Website which was one of the top 3 projects in the SEP Expo 2023 for Grade 10.",
         cardID: "sep10Card",
         repo: "https://github.com/samuels0052/sep10-freedom-project",
         view: "samuels0052.github.io/sep10-freedom-project/",
       },
       {
         title: "LBH Blackhole Game",
-        description:
-          "Explore different parts of a blackhole in a 2D Unity Game.",
         cardID: "lbhbhgCard",
         repo: "https://github.com/samuels0052/LBH-Game",
         view: "https://samuels0052.github.io/LBH-Game/",
       },
       {
         title: "PigLatin Translator",
-        description: "Translate English into Piglatin and vice versa.",
         cardID: "pltCard",
-        repo: `https://github.com/${githubUsername}/PigLatin`,
+        repo: "https://github.com/code2cube/PigLatin",
         view: "https://piglatin.pages.dev/",
       },
       {
         title: "Ice Cube Bot",
-        description: "Winter themed PVP Bot made for Snow Codes Bot Jam.",
         cardID: "placeholder",
-        repo: `https://github.com/${githubUsername}/Ice-Cube-Bot`,
+        repo: "https://github.com/code2cube/Ice-Cube-Bot",
       },
       {
         title: "Eris Bot Template",
-        description: "Template for an Eris.js based Discord Bot.",
         cardID: "ebtCard",
-        repo: `https://github.com/${githubUsername}/Eris-Bot-Template`,
+        repo: "https://github.com/code2cube/Eris-Bot-Template",
       },
     ];
 
-    const sortedProjects = projects.sort((a, b) =>
+    const promises = projects.map(async (i) => {
+      const repoMatch = i.repo.match(/[^/]*$/);
+      const ownerMatch = i.repo.match(/\/([^\/]*)\/[^\/]*$/);
+      if (repoMatch && ownerMatch) {
+        const repo = repoMatch[0];
+        const owner = ownerMatch[1];
+        i.description = await getDescription(owner, repo);
+      } else {
+        i.description = "No Description Found";
+      }
+      return i;
+    });
+
+    const updatedProjects = await Promise.all(promises);
+
+    const sortedProjects = updatedProjects.sort((a, b) =>
       a.title.localeCompare(b.title)
     );
 
