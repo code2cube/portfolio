15c029dacac6e22982c4c293e180950794b71a3e
Samuel Sharivker <sjsharivker@gmail.com>
2024-08-01 23:09:37 -0400

    home page mvp done

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,45 @@
 "use client";
 
+import { HeroParallax } from "@/components/ui/hero-parallax";
+import { useState, useEffect } from "react";
+
+interface fetchProject {
+  title: string;
+  cardID: string;
+  repo: string;
+}
+
+interface Project {
+  title: string;
+  link: string;
+  thumbnail: string;
+}
+
 export default function Home() {
+  const [projects, setProjects] = useState<Project[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch("/api/projects");
+      const result = await response.json();
+
+      const convertedProject: Project[] = result.projects.map(
+        (i: fetchProject) => ({
+          title: i.title,
+          link: i.repo,
+          thumbnail: i.cardID,
+        })
+      );
+
+      setProjects(convertedProject);
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div>
-      <h1 className="text-white">Home Page</h1>
+      <HeroParallax products={projects} />
     </div>
   );
 }
