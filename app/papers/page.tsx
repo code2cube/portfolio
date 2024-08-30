19cc8bb1c9d522553521e8702bb83d9d72ff0cd8
Samuel Sharivker <sam@ShmulPro.local>
2024-08-30 16:59:00 -0400

    updated footer

--- app/papers/page.tsx
+++ app/papers/page.tsx
@@ -1,9 +1,16 @@
 "use client";
 
+import React from "react";
+import { LinkPreview } from "@/components/ui/link-preview";
+import { ShootingStars } from "@/components/ui/shooting-stars";
+import { StarsBackground } from "@/components/ui/stars-background";
+
 export default function Papers() {
   return (
-    <div className="text-white">
-      <p>Papers</p>
+    <div className="relative z-0">
+      <ShootingStars className="absolute inset-0 z-0" />
+      <StarsBackground className="absolute inset-0 z-0" />
+      <div className="relative text-white min-h-screen z-50 flex flex-col items-center justify-center gap-3"></div>
     </div>
   );
 }
--- components/footer.tsx
+++ components/footer.tsx
@@ -1,7 +1,6 @@
 "use client";
 
-import Link from "next/link";
-
+import { LinkPreview } from "@/components/ui/link-preview";
 interface footerLinks {
   link: string;
   icon: string;
@@ -9,11 +8,11 @@ interface footerLinks {
 
 export default function Footer() {
   const links: footerLinks[] = [
-    { link: "https://github.com/samSharivker/", icon: "bi-github" },
-    { link: "https://www.instagram.com/live2cube", icon: "bi-instagram" },
+    { link: "https://github.com/samSharivker/", icon: "github" },
+    { link: "https://www.instagram.com/live2cube", icon: "instagram" },
     {
       link: "https://www.linkedin.com/in/samuel-sharivker/",
-      icon: "bi-linkedin",
+      icon: "linkedin",
     },
   ];
 
@@ -21,11 +20,13 @@ export default function Footer() {
     <footer className="bg-black text-white p-5 flex flex-col h-20 text-center justify-center items-center gap-1 z-50">
       <div className="flex gap-3">
         {links.map((item) => (
-          <Link key={item.link} href={item.link}>
-            <i
-              className={`bi ${item.icon} hover:text-red-700 transition ease-in duration-200`}
-            ></i>
-          </Link>
+          <LinkPreview
+            key={item.link}
+            url={item.link}
+            className="font-bold bg-clip-text text-transparent bg-gradient-to-br transition ease-in hover:scale-150 hover:text-red-700 text-white duration-200"
+          >
+            <i className={`bi bi-${item.icon}`}></i>
+          </LinkPreview>
         ))}
       </div>
       <p className="text-xs">Copyright © Samuel Sharivker 2024</p>
