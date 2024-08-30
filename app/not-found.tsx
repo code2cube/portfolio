e4af4ca52836c40c19f1222a09d8c379acebe14c
Samuel Sharivker <sam@ShmulPro.local>
2024-08-30 15:56:05 -0400

    updated not-found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,9 +1,26 @@
 "use client";
 
+import { TransitionLink } from "@/components/TransitionLink";
+import { ShootingStars } from "@/components/ui/shooting-stars";
+import { StarsBackground } from "@/components/ui/stars-background";
+
 export default function _404Page() {
   return (
-    <div className="text-white">
-      <h1>This page does not exist...</h1>
+    <div className="relative z-0">
+      <ShootingStars className="absolute inset-0 z-0" />
+      <StarsBackground className="absolute inset-0 z-0" />
+      <div className="relative text-white min-h-screen z-50 flex flex-col items-center justify-center gap-3">
+        <p className="md:text-9xl sm:text-7xl text-5xl text-red-700">404</p>
+        <p className="md:text-5xl sm:text-3xl text-lg">
+          This Page Does not Exist!
+        </p>
+        <TransitionLink
+          href="/"
+          className="bg-white text-black p-3 rounded-lg transition hover:scale-110 ease-in"
+        >
+          Home
+        </TransitionLink>
+      </div>
     </div>
   );
 }
